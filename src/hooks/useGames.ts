9a597a7e05095157500a6762662d735f16d6beba
Afsaneh Mollasalehi <salehi.afsane23@gmail.com>
2024-03-06 12:59:31 +0100

    Implement searching.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,7 +24,8 @@ const useGames = (gamequery:GameQuery) => useData<Game>('/games',
   {
     genres:gamequery.genre?.id,
     platforms: gamequery.platform?.id,
-    ordering: gamequery.sortOrder
+    ordering: gamequery.sortOrder,
+    search:gamequery.searchText
     }
 }
 ,[gamequery])
